bd57cfdea044a2ee0b7fabdf8882f6acda882e2d
nicofigueredo <nico_1507@live.com.ar>
2018-10-31 20:44:05 -0300

    Se agregaron nuevos tests para probar las modificaciones realizadas

--- Snake 0.3.2/test/edu/unlam/tpa/test/TestJuego.java
+++ Snake 0.3.2/test/edu/unlam/tpa/test/TestJuego.java
@@ -15,181 +15,273 @@ import edu.unlam.tpa.Snake;
 
 public class TestJuego {
 	
-//	Snake snake;
-//	
+	Snake snake;
+		
+	@Test
+	public void seMueveCabeza() {
+		snake = new Snake(0, 0, Direccion.DERECHA);
+		snake.moverse();
+		
+		ArrayList<Posicion> cuerpo = new ArrayList<>();
+		cuerpo.add(new Posicion(0, 1));
+		assertEquals(cuerpo, snake.getCuerpo());
+	}
+	
+	@Test
+	public void crece() {
+		snake = new Snake(0, 0, Direccion.DERECHA);
+		
+		snake.crecer();
+		snake.crecer();
+		snake.crecer();
+		
+		ArrayList<Posicion> cuerpo = new ArrayList<>();
+		cuerpo.add(0, new Posicion(0, 0));
+		cuerpo.add(0, new Posicion(0, 1));
+		cuerpo.add(0, new Posicion(0, 2));
+		cuerpo.add(0, new Posicion(0, 3));	//kbza
+		
+		assertEquals(cuerpo, snake.getCuerpo());
+	}
+		
+	@Test
+	public void seMueveCuerpo() {
+		snake = new Snake(0, 0, Direccion.DERECHA);
+		
+		snake.crecer();
+		snake.crecer();
+		snake.crecer();
+		snake.moverse();
+		snake.cambiarDireccion(Direccion.ABAJO);
+		snake.moverse();
+		
+		ArrayList<Posicion> cuerpo = new ArrayList<>();
+		cuerpo.add(0, new Posicion(0, 2));
+		cuerpo.add(0, new Posicion(0, 3));
+		cuerpo.add(0, new Posicion(0, 4));
+		cuerpo.add(0, new Posicion(1, 4));	//kbza
+		
+		assertEquals(cuerpo, snake.getCuerpo());
+	}
+	
+	Mapa mapa;
+	Partida partida;
+	
+	@Test
+	public void muereBordeArriba() {
+		snake = new Snake(0, 0, Direccion.DERECHA);
+		partida = new Partida(new Mapa(10, 10));
+		partida.addSnake(snake);
+		
+		partida.actualizarPartida();
+		snake.cambiarDireccion(Direccion.ARRIBA);
+		partida.actualizarPartida();
+		
+		assertEquals(false, snake.estaViva());
+	}
+	
+	@Test
+	public void muereBordeIzquierdo() {
+		snake = new Snake(2, 0, Direccion.ABAJO);
+		partida = new Partida(new Mapa(10, 10));
+		partida.addSnake(snake);
+		
+		partida.actualizarPartida();
+		snake.cambiarDireccion(Direccion.IZQUIERDA);
+		partida.actualizarPartida();
+		
+		assertEquals(false, snake.estaViva());
+	}
+	
+	@Test
+	public void muereBordeAbajo() {
+		snake = new Snake(9, 1, Direccion.DERECHA);
+		partida = new Partida(new Mapa(10, 10));
+		partida.addSnake(snake);
+		
+		partida.actualizarPartida();
+		snake.cambiarDireccion(Direccion.ABAJO);
+		partida.actualizarPartida();
+		
+		assertEquals(false, snake.estaViva());
+	}
+	
+	@Test
+	public void muereBordeDerecho() {
+		snake = new Snake(9, 3, Direccion.ABAJO);
+		partida = new Partida(new Mapa(10, 10));
+		partida.addSnake(snake);
+		
+		partida.actualizarPartida();
+		snake.cambiarDireccion(Direccion.DERECHA);
+		partida.actualizarPartida();
+		
+		assertEquals(false, snake.estaViva());
+	}
+	
+	Fruta fruta;
+	@Test
+	public void comeYcrece() {
+		snake = new Snake(0, 0, Direccion.DERECHA);
+		fruta = new Fruta(0, 2);
+		partida = new Partida(new Mapa(10, 10));
+		partida.addSnake(snake);
+		partida.addFruta(fruta);
+		
+		partida.actualizarPartida();
+		partida.actualizarPartida();
+		
+		ArrayList<Posicion> cuerpo = new ArrayList<>();
+		cuerpo.add(0, new Posicion(0, 1));
+		cuerpo.add(0, new Posicion(0, 2));	//kbza
+		assertEquals(cuerpo, snake.getCuerpo());
+	}
+	
 //	@Test
-//	public void seMueveCabeza() {
-//		snake = new Snake(0, 0, Direccion.DERECHA);
-//		snake.moverse();
-//		
-//		ArrayList<Posicion> cuerpo = new ArrayList<>();
-//		cuerpo.add(new Posicion(0, 1));
-//		
-//		assertEquals(cuerpo, snake.getCuerpo());
-//	}
-//	
+	public void noChocaConsigoMismaLoop() {
+		snake = new Snake(3, 3, Direccion.DERECHA);
+		partida = new Partida(new Mapa(10, 10));
+		partida.addSnake(snake);
+		
+		snake.crecer();
+		snake.crecer();
+		snake.crecer();
+		snake.cambiarDireccion(Direccion.ABAJO);
+		partida.actualizarPartida(true);
+		
+
+		snake.cambiarDireccion(Direccion.IZQUIERDA);
+		partida.actualizarPartida(true);
+		
+
+		snake.cambiarDireccion(Direccion.ARRIBA);
+		assertEquals(true, snake.estaViva());
+		partida.actualizarPartida(true);
+		
+
+		assertEquals(true, snake.estaViva());
+		snake.cambiarDireccion(Direccion.DERECHA);
+		partida.actualizarPartida(true);
+		
+
+		assertEquals(true, snake.estaViva());
+		snake.cambiarDireccion(Direccion.ABAJO);
+		partida.actualizarPartida(true);
+	}
+	
+	Snake snake2;
+	@Test
+	public void chocanCabezas() {
+		snake = new Snake(0, 1, Direccion.DERECHA);
+		snake2 = new Snake(0, 5, Direccion.IZQUIERDA);
+		partida = new Partida(new Mapa(10, 10));
+		partida.addSnake(snake);
+		partida.addSnake(snake2);
+		
+		partida.actualizarPartida();
+		assertEquals(true, snake.estaViva());
+		assertEquals(true, snake2.estaViva());
+		partida.actualizarPartida();
+		assertEquals(false, snake.estaViva());
+		assertEquals(false, snake2.estaViva());
+	}
+	
 //	@Test
-//	public void crece() {
-//		snake = new Snake(0, 0, Direccion.DERECHA);
-//		
-//		snake.crecer();
-//		snake.crecer();
-//		snake.crecer();
-//		
-//		ArrayList<Posicion> cuerpo = new ArrayList<>();
-//		cuerpo.add(0, new Posicion(0, 0));
-//		cuerpo.add(0, new Posicion(0, 1));
-//		cuerpo.add(0, new Posicion(0, 2));
-//		cuerpo.add(0, new Posicion(0, 3));	//kbza
-//		
-//		assertEquals(cuerpo, snake.getCuerpo());
-//	}
-//		
+	public void casiChocanmuereUnaDelasDos() {
+		snake = new Snake(0, 1, Direccion.DERECHA);
+		snake2 = new Snake(0, 5, Direccion.IZQUIERDA);
+		partida = new Partida(new Mapa(10, 10));
+		partida.addSnake(snake);
+		partida.addSnake(snake2);
+		snake.crecer();
+		
+		partida.actualizarPartida(true);
+		assertEquals(true, snake.estaViva());
+		assertEquals(true, snake2.estaViva());
+		snake.cambiarDireccion(Direccion.ABAJO);
+		partida.actualizarPartida(true);
+		
+		partida.actualizarPartida(true);
+		assertEquals(true, snake.estaViva());
+		assertEquals(true, snake.estaViva());
+		assertEquals(false, snake2.estaViva());
+		partida.actualizarPartida(true);
+	}
+	
 //	@Test
-//	public void seMueveCuerpo() {
-//		snake = new Snake(0, 0, Direccion.DERECHA);
-//		
-//		snake.crecer();
-//		snake.crecer();
-//		snake.crecer();
-//		snake.moverse();
-//		snake.cambiarDireccion(Direccion.ABAJO);
-//		snake.moverse();
-//		
-//		ArrayList<Posicion> cuerpo = new ArrayList<>();
-//		cuerpo.add(0, new Posicion(0, 2));
-//		cuerpo.add(0, new Posicion(0, 3));
-//		cuerpo.add(0, new Posicion(0, 4));
-//		cuerpo.add(0, new Posicion(1, 4));	//kbza
-//		
-//		assertEquals(cuerpo, snake.getCuerpo());
-//	}
-//	
-//	Mapa mapa;
-//	Partida partida;
-//	
+	public void loopean() {
+		snake = new Snake(0, 1, Direccion.DERECHA);
+		snake2 = new Snake(0, 3, Direccion.DERECHA);
+		partida = new Partida(new Mapa(10, 10));
+		partida.addSnake(snake);
+		partida.addSnake(snake2);
+		snake.crecer();
+		snake2.crecer();
+		partida.actualizarPartida(true);
+		partida.actualizarPartida(true);
+		snake2.cambiarDireccion(Direccion.ABAJO);
+		partida.actualizarPartida(true);
+		partida.actualizarPartida(true);
+	}
+	
 //	@Test
-//	public void muereBordeArriba() {
-//		snake = new Snake(0, 0, Direccion.DERECHA);
-//		partida = new Partida(new Mapa(10, 10));
-//		partida.addSnake(snake);
-//		
-//		partida.actualizarPartida();
-//		snake.cambiarDireccion(Direccion.ARRIBA);
-//		partida.actualizarPartida();
-//		
-//		assertEquals(false, snake.estaViva());
-//	}
-//	
+	public void loopeanYunoCome() {
+		snake = new Snake(0, 1, Direccion.DERECHA);
+		snake2 = new Snake(0, 3, Direccion.DERECHA);
+		fruta = new Fruta(0, 6);
+		partida = new Partida(new Mapa(10, 10));
+		partida.addSnake(snake);
+		partida.addSnake(snake2);
+		partida.addFruta(fruta);
+		snake.crecer();
+		snake2.crecer();
+		partida.actualizarPartida(true);
+		partida.actualizarPartida(true);
+		partida.actualizarPartida(true);
+		partida.actualizarPartida(true);
+		partida.actualizarPartida(true);
+	}
+	
 //	@Test
-//	public void muereBordeIzquierdo() {
-//		snake = new Snake(2, 0, Direccion.ABAJO);
-//		partida = new Partida(new Mapa(10, 10));
-//		partida.addSnake(snake);
-//		
-//		partida.actualizarPartida();
-//		snake.cambiarDireccion(Direccion.IZQUIERDA);
-//		partida.actualizarPartida();
-//		
-//		assertEquals(false, snake.estaViva());
-//	}
-//	
-//	@Test
-//	public void muereBordeAbajo() {
-//		snake = new Snake(9, 1, Direccion.DERECHA);
-//		partida = new Partida(new Mapa(10, 10));
-//		partida.addSnake(snake);
-//		
-//		partida.actualizarPartida();
-//		snake.cambiarDireccion(Direccion.ABAJO);
-//		partida.actualizarPartida();
-//		
-//		assertEquals(false, snake.estaViva());
-//	}
-//	
-//	@Test
-//	public void muereBordeDerecho() {
-//		snake = new Snake(9, 3, Direccion.ABAJO);
-//		partida = new Partida(new Mapa(10, 10));
-//		partida.addSnake(snake);
-//		
-//		partida.actualizarPartida();
-//		snake.cambiarDireccion(Direccion.DERECHA);
-//		partida.actualizarPartida();
-//		
-//		assertEquals(false, snake.estaViva());
-//	}
-//	
-//	Fruta fruta;
-//	@Test
-//	public void comeYcrece() {
-//		snake = new Snake(0, 0, Direccion.DERECHA);
-//		fruta = new Fruta(0, 2);
-//		partida = new Partida(new Mapa(10, 10));
-//		partida.addSnake(snake);
-//		partida.addFruta(fruta);
-//		
-//		partida.actualizarPartida();
-//		partida.actualizarPartida();
-//		
-//		ArrayList<Posicion> cuerpo = new ArrayList<>();
-//		cuerpo.add(0, new Posicion(0, 1));
-//		cuerpo.add(0, new Posicion(0, 2));	//kbza
-//		assertEquals(cuerpo, snake.getCuerpo());
-//	}
-//	
-//	@Test
-//	public void chocaConsigoMisma() {
-//		snake = new Snake(3, 3, Direccion.DERECHA);
-//		partida = new Partida(new Mapa(10, 10));
-//		partida.addSnake(snake);
-//		
-//		snake.crecer();
-//		snake.crecer();
-//		snake.crecer();
-//		snake.cambiarDireccion(Direccion.ABAJO);
-//		partida.actualizarPartida();
-//		snake.cambiarDireccion(Direccion.IZQUIERDA);
-//		partida.actualizarPartida();
-//		snake.cambiarDireccion(Direccion.ARRIBA);
-//		assertEquals(true, snake.estaViva());
-//		partida.actualizarPartida();
-//		assertEquals(false, snake.estaViva());
-//	}
-//	
-//	Snake snake2;
-//	@Test
-//	public void chocanCabezas() {
-//		snake = new Snake(0, 1, Direccion.DERECHA);
-//		snake2 = new Snake(0, 5, Direccion.IZQUIERDA);
-//		partida = new Partida(new Mapa(10, 10));
-//		partida.addSnake(snake);
-//		partida.addSnake(snake2);
-//		
-//		partida.actualizarPartida();
-//		assertEquals(true, snake.estaViva());
-//		assertEquals(true, snake2.estaViva());
-//		partida.actualizarPartida();
-//		assertEquals(false, snake.estaViva());
-//		assertEquals(false, snake2.estaViva());
-//	}
-//	
-//	@Test
-//	public void casiChocanmuereUnaDelasDos() {
-//		snake = new Snake(0, 1, Direccion.DERECHA);
-//		snake2 = new Snake(0, 5, Direccion.IZQUIERDA);
-//		partida = new Partida(new Mapa(10, 10));
-//		partida.addSnake(snake);
-//		partida.addSnake(snake2);
-//		
-//		snake.crecer();
-//		partida.actualizarPartida();
-//		assertEquals(true, snake.estaViva());
-//		assertEquals(true, snake2.estaViva());
-//		snake.cambiarDireccion(Direccion.ABAJO);
-//		partida.actualizarPartida();
-//		assertEquals(true, snake.estaViva());
-//		assertEquals(false, snake2.estaViva());
-//	}
+	public void loopeanCola() {
+		snake = new Snake(2, 1, Direccion.DERECHA);
+		snake2 = new Snake(4, 3, Direccion.IZQUIERDA);
+		
+		partida = new Partida(new Mapa(10, 10));
+		partida.addSnake(snake);
+		partida.addSnake(snake2);
+		
+		snake.crecer();
+		snake2.crecer();
+		snake.crecer();
+		snake2.crecer();
+		snake.cambiarDireccion(Direccion.ABAJO);
+		snake2.cambiarDireccion(Direccion.ARRIBA);
+		snake.crecer();
+		snake2.crecer();
+		partida.actualizarPartida(true);
+		snake.cambiarDireccion(Direccion.IZQUIERDA);
+		snake2.cambiarDireccion(Direccion.DERECHA);
+		partida.actualizarPartida(true);
+		partida.actualizarPartida(true);
+		partida.actualizarPartida(true);
+		partida.actualizarPartida(true);
+	}
+	
+	@Test
+	public void chocanCabezasPerpendicularmente()  {
+		snake = new Snake(2, 1, Direccion.DERECHA);
+		snake2 = new Snake(4, 3, Direccion.ARRIBA);
+		
+		partida = new Partida(new Mapa(10, 10));
+		partida.addSnake(snake);
+		partida.addSnake(snake2);
+		
+		snake.crecer();
+		snake2.crecer();
+		
+		partida.actualizarPartida(true);
+		partida.actualizarPartida(true);
+	}
 		
 }
