70da485d1e6dd305c1f65d79b4457bd69bffc2c1
nicofigueredo <nico_1507@live.com.ar>
2018-11-06 13:54:59 -0300

    Se modifica el metodo morir() para que respawnee y se agrega el metodo paint()

--- Snake 0.3.2/src/edu/unlam/tpa/Fruta.java
+++ Snake 0.3.2/src/edu/unlam/tpa/Fruta.java
@@ -1,9 +1,13 @@
 package edu.unlam.tpa;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.util.Random;
+
 public class Fruta {
 
 	private Posicion pos;
-	
+
 	public Fruta(int x, int y) {
 		this.pos = new Posicion(x, y);
 	}
@@ -15,12 +19,28 @@ public class Fruta {
 	public Posicion getPos() {
 		return pos;
 	}
-	
+
 	public boolean estasAhi(Posicion pos) {
 		return this.pos.equals(pos);
 	}
 
-	public void morir() {		
+	public void morir(Mapa mapa) {
+		Random rand = new Random();
+		boolean ok = false;
+		while (!ok) {
+			int x = rand.nextInt(mapa.getFil() - 1) + 1;
+			int y = rand.nextInt(mapa.getCol() - 1) + 1;
+			if (!mapa.hayVibora(new Posicion(x, y), null)) {
+				this.pos.x = x;
+				this.pos.y = y;
+				ok = true;
+			}
+		}
 	}
-	
+
+	public void paint(Graphics2D g2, int tileSize) {
+		g2.setColor(Color.RED);
+		g2.fillRect(pos.x * tileSize, pos.y * tileSize, tileSize, tileSize);
+	}
+
 }
