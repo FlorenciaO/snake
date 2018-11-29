5002f1c04190efbd8689f040044f785335cecf34
FlorenciaO <o.florencia123@gmail.com>
2018-11-28 23:58:01 -0300

    No se debe entrar a mas de una sala a la vez

--- Snake 0.3.2/src_cliente/edu/unlam/tpa_COMANDOSESCUCHASERVER/EntrarSala.java
+++ Snake 0.3.2/src_cliente/edu/unlam/tpa_COMANDOSESCUCHASERVER/EntrarSala.java
@@ -17,13 +17,13 @@ public class EntrarSala extends ComandoEscuchaServer {
 		PaqueteSala paqueteSala = gson.fromJson(cadenaLeida, PaqueteSala.class);
 		cliente.setPaqueteSala(paqueteSala);
 		if (paqueteSala.getMsj().equals(Paquete.msjExito)) {
-			if (!cliente.getSalasActivas().containsKey(paqueteSala.getNombreSala())) {				
+			if (cliente.getSalasActivas().size() == 0) {				
 				VentanaSala sala = new VentanaSala(cliente);
 				
 				cliente.getSalasActivas().put(cliente.getPaqueteSala().getNombreSala(), sala);
 				escuchaServer.actualizarListaConectadosSala(paqueteSala);
 			} else {
-				JOptionPane.showMessageDialog(null, "Ya se encuentra conectado a esta sala");
+				JOptionPane.showMessageDialog(null, "Ya estas dentro de una sala");
 			} 
 		} else {
 			JOptionPane.showMessageDialog(null, "Error al intentar entrar en la sala " + cliente.getPaqueteSala().getNombreSala());
