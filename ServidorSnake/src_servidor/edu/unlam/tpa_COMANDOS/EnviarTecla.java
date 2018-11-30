184f6e684244ea6e98ac7ddbe67974b1b08eefc5
FlorenciaO <o.florencia123@gmail.com>
2018-11-30 17:10:05 -0300

    Iniciar partida

---
+++ ServidorSnake/src_servidor/edu/unlam/tpa_COMANDOS/EnviarTecla.java
@@ -0,0 +1,31 @@
+package edu.unlam.tpa_COMANDOS;
+
+import edu.unlam.tpa_COMUNICACION.Servidor;
+import edu.unlam.tpa_PAQUETESCLIENTE.PaqueteTecla;
+import edu.unlam.tpa_UTILES.HiloPartida;
+
+public class EnviarTecla extends ComandoServer {
+
+	@Override
+	public void ejecutar() {
+		
+		PaqueteTecla paqueteTecla = (PaqueteTecla) (gson.fromJson(cadenaLeida, PaqueteTecla.class));
+		boolean encontreJugador = false;
+		try
+		{
+			for(HiloPartida partida: Servidor.partidas) {
+				if(partida.buscarJugadorYcambiarTecla(paqueteTecla.getNombreJugador(), paqueteTecla.getTeclaPresionada()) {
+					encontreJugador = true;
+					break;
+				}		
+			}
+			if(encontreJugador) {
+				throw new Exception("No existe jugador en ninguna partida");
+			}
+			
+		} catch(Exception e) {
+			Servidor.getLog().append(e.getMessage());
+		}
+	}
+
+}
